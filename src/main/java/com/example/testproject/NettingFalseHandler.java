101895c405ba5dd81063e81b08b02613bc10db72
codekamit <kamitstar@icloud.com>
2024-10-26 02:01:08 +0530

    tewst

--- src/main/java/com/example/testproject/NettingFalseHandler.java
+++ src/main/java/com/example/testproject/NettingFalseHandler.java
@@ -3,7 +3,7 @@ package com.example.testproject;
 import java.util.List;
 import java.util.Map;
 
-public class NettingHandler extends BaseHandler<NettingInputDTO, List<NetEarning>> {
+public class NettingHandler extends BaseHandler<NettingInputDTO , List<NetEarning>> {
     private ClientConfigService clientConfigService;
     private BaseHandler next;
     private
@@ -11,7 +11,21 @@ public class NettingHandler extends BaseHandler<NettingInputDTO, List<NetEarning
     public List<NetEarning> handle(NettingInputDTO nettingInputDTO) {
         if(nettingInputDTO.getClientPreferenceDTO().getNetting().equals(false) &&
                 nettingInputDTO.getClientPreferenceDTO().getGroupingType().equals(GroupingType.PAYMENT_FUND))
-
-
+            return NettingScenarios.noNettingPaymentFundLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()), nettingInputDTO.getFundGroupDTO());
+        else if(nettingInputDTO.getClientPreferenceDTO().getNetting().equals(false) &&
+                nettingInputDTO.getClientPreferenceDTO().getGroupingType().equals(GroupingType.FUND))
+            return NettingScenarios.noNettingFundLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()));
+        else if(nettingInputDTO.getClientPreferenceDTO().getNetting().equals(false) &&
+                nettingInputDTO.getClientPreferenceDTO().getGroupingType().equals(GroupingType.CLIENT))
+            return NettingScenarios.noNettingClientLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()), nettingInputDTO.getFundGroupDTO());
+        else if(nettingInputDTO.getClientPreferenceDTO().getNetting().equals(true) &&
+                nettingInputDTO.getClientPreferenceDTO().getGroupingType().equals(GroupingType.PAYMENT_FUND))
+            return NettingScenarios.NettingPaymentFundLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()), nettingInputDTO.getFundGroupDTO());
+        else if(nettingInputDTO.getClientPreferenceDTO().getNetting().equals(true) &&
+                nettingInputDTO.getClientPreferenceDTO().getGroupingType().equals(GroupingType.FUND))
+            return NettingScenarios.NettingFundLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()));
+        else {
+            return NettingScenarios.NettingClientLevel(nettingInputDTO.getClientEarnings().get(nettingInputDTO.getClientPreferenceDTO().getClientShortName()), nettingInputDTO.getFundGroupDTO());
+        }
     }
 }
--- src/main/java/com/example/testproject/NettingScenarios.java
+++ src/main/java/com/example/testproject/NettingScenarios.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public interface NettingScenarios {
@@ -69,15 +70,48 @@ public interface NettingScenarios {
             netEarning.setClientShortName(earnings.get(0).getClientShortName());
             netEarning.setTotalAmount(earnings.stream()
                     .map(Earning::getAmount)
-                    .reduce((x, y) -> x + y).get());
+                    .reduce(Double::sum).get());
+            netEarnings.add(netEarning);
         });
+        return netEarnings;
     }
 
-    public static List<NetEarning> NettingFundLevel() {
-
+    public static List<NetEarning> NettingFundLevel(List<Earning> clientEarnings) {
+        List<NetEarning> netEarnings = new ArrayList<>();
+        Map<String, List<Earning>> clientEarningsMap = clientEarnings.stream()
+                .collect(Collectors.groupingBy(Earning::getFund));
+        clientEarningsMap.forEach((fund, earnings) -> {
+            NetEarning netEarning = new NetEarning();
+            netEarning.setClientShortName(earnings.get(0).getClientShortName());
+            netEarning.setCurrency(earnings.get(0).getCurrency());
+            netEarning.setPayableAt(fund);
+            netEarning.setTotalAmount(earnings.stream()
+                    .map(Earning::getAmount)
+                    .reduce(Double::sum).get());
+            netEarnings.add(netEarning);
+        });
+        return netEarnings;
     }
 
-    public static List<NetEarning> NettingClientLevel() {
-
+    public static List<NetEarning> NettingClientLevel(List<Earning> clientEarnings, FundGroupDTO fundGroupDTO) {
+        Map<String, List<Earning>> earningsFundMap = new HashMap<>();
+        clientEarnings.forEach(earning -> {
+            if(!earningsFundMap.containsKey(fundGroupDTO.getFundGroupMap().get(earning.getFund()))) {
+                earningsFundMap.put(fundGroupDTO.getFundGroupMap().get(earning.getFund()), new ArrayList<>());
+            }
+            earningsFundMap.get(fundGroupDTO.getFundGroupMap().get(earning.getFund())).add(earning);
+        });
+        List<NetEarning> netEarnings = new ArrayList<>();
+        earningsFundMap.forEach((paymentFund, earnings) -> {
+            NetEarning netEarning = new NetEarning();
+            netEarning.setPayableAt(paymentFund);
+            netEarning.setCurrency(earnings.get(0).getCurrency());
+            netEarning.setClientShortName(earnings.get(0).getClientShortName());
+            netEarning.setTotalAmount(earnings.stream()
+                    .map(Earning::getAmount)
+                    .reduce(Double::sum).get());
+            netEarnings.add(netEarning);
+        });
+        return netEarnings;
     }
 }
