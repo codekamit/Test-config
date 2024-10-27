5d24df8be15562f10c06d1813f398d2cc11fa591
codekamit <kamitstar@icloud.com>
2024-10-27 23:31:31 +0530

    Test Commit

--- src/main/java/com/example/testproject/TestProjectApplication.java
+++ src/main/java/com/example/testproject/TestProjectApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestProjectApplication {
     public static void main(String[] args) {
-        var context = SpringApplication.run(TestProjectApplication.class, args);
-//        ClientConfigService clientConfigService = context.getBean(ClientConfigService.class);
-//        Preprocessor preprocessor = context.getBean(Preprocessor.class);
-//        preprocessor.process(clientConfigService.getTestEarnings());
+        SpringApplication.run(TestProjectApplication.class, args);
     }
 }
